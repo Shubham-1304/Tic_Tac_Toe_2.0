c1bccb6cae87b60f92a0456889302b076b3f3185
shubh123 <shubh@example.com>
2022-01-29 13:03:54 +0530

    game end on coin becomes zero fixed

--- app/src/main/java/com/example/connect3/Board.java
+++ app/src/main/java/com/example/connect3/Board.java
@@ -1,17 +1,13 @@
 package com.example.connect3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import androidx.gridlayout.widget.GridLayout;
-
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,16 +15,15 @@ import android.widget.Toast;
 import com.mikhaellopez.circularimageview.CircularImageView;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 import nl.dionsegijn.konfetti.KonfettiView;
 import nl.dionsegijn.konfetti.models.Shape;
 import nl.dionsegijn.konfetti.models.Size;
 
-public class MainActivity extends AppCompatActivity {
-    /*Button play;
+public class Board extends AppCompatActivity {
+    int alternative;
+    Button play;
     TextView text;
     int n=0;
     int w=0;
@@ -39,11 +34,14 @@ public class MainActivity extends AppCompatActivity {
     int coinPicked=0;
     int tag;
     int coinCount=0;
+    String name1;
+    String name2;
 
     ImageView coin;
     int tagNo=0;
     public void coinchose(View view){
         coin=(ImageView) view;
+
         //coin.getDrawable().toString()
         tag=Integer.parseInt(coin.getTag().toString());
         if ((tagNo%2==0 && Integer.parseInt(coin.getTag().toString())%2==0) || (tagNo%2!=0 && Integer.parseInt(coin.getTag().toString())%2!=0)) {
@@ -92,13 +90,14 @@ public class MainActivity extends AppCompatActivity {
                 for (int[] wins : win) {
                     if (state[wins[0]] == state[wins[1]] && state[wins[1]] == state[wins[2]] && state[wins[0]] == 0 && w == 0) {
                         winner = "Red";
-                        text.setText(winner + " has won ");
+                        text.setText(name1 + " has won ");
+                        play.setText("PLAY AGAIN");
                         text.setVisibility(View.VISIBLE);
                         play.setVisibility(View.VISIBLE);
                         w = 1;
                         Random ran = new Random();
                         int x = ran.nextInt(pallete.length);
-                        text.setTextColor(this.getResources().getColor(pallete[x]));
+                        //text.setTextColor(this.getResources().getColor(pallete[x]));
                         KonfettiView konfettiView = findViewById(R.id.viewKonfetti);
                         konfettiView.build()
                                 .addColors( Color.MAGENTA,Color.RED)
@@ -114,13 +113,14 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     } else if (state[wins[0]] == state[wins[1]] && state[wins[1]] == state[wins[2]] && state[wins[0]] == 1 && w == 0) {
                         winner = "Yellow";
-                        text.setText(winner + " has won ");
+                        text.setText(name2 + " has won ");
+                        play.setText("PLAY AGAIN");
                         text.setVisibility(View.VISIBLE);
                         play.setVisibility(View.VISIBLE);
                         w = 1;
                         Random ran = new Random();
                         int x = ran.nextInt(pallete.length);
-                        text.setTextColor(this.getResources().getColor(pallete[x]));
+                        //text.setTextColor(this.getResources().getColor(pallete[x]));
                         KonfettiView konfettiView = findViewById(R.id.viewKonfetti);
                         konfettiView.build()
                                 .addColors(Color.YELLOW)
@@ -147,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
                 c8.setTranslationY(-1000);
                 c8.animate().translationYBy(1000).setDuration(500);
             }else if (state[tapped]!=2 ){
-                if ((int)Array.get(tagPostion,tapped)%2==0 && tag>(int)Array.get(tagPostion,tapped) && (tag-(int)Array.get(tagPostion,tapped))>=3 && (tag-(int)Array.get(tagPostion,tapped))%2!=0) {
+                if ((int) Array.get(tagPostion,tapped)%2==0 && tag>(int)Array.get(tagPostion,tapped) && (tag-(int)Array.get(tagPostion,tapped))>=3 && (tag-(int)Array.get(tagPostion,tapped))%2!=0) {
                     Toast.makeText(this, "coin override", Toast.LENGTH_SHORT).show();
                     tagNo=Integer.parseInt(coin.getTag().toString());
                     c8.setImageResource(R.drawable.red);
@@ -179,7 +179,8 @@ public class MainActivity extends AppCompatActivity {
                 for (int[] wins : win) {
                     if (state[wins[0]] == state[wins[1]] && state[wins[1]] == state[wins[2]] && state[wins[0]] == 0 && w == 0) {
                         winner = "Red";
-                        text.setText(winner + " has won ");
+                        text.setText(name1  + " has won ");
+                        play.setText("PLAY AGAIN");
                         text.setVisibility(View.VISIBLE);
                         play.setVisibility(View.VISIBLE);
                         w = 1;
@@ -202,7 +203,8 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     } else if (state[wins[0]] == state[wins[1]] && state[wins[1]] == state[wins[2]] && state[wins[0]] == 1 && w == 0) {
                         winner = "Yellow";
-                        text.setText(winner + " has won ");
+                        text.setText(name2  + " has won ");
+                        play.setText("PLAY AGAIN");
                         text.setVisibility(View.VISIBLE);
                         play.setVisibility(View.VISIBLE);
                         w = 1;
@@ -239,6 +241,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void again(View view){
+        alternative++;
+        //Toast.makeText(this, alternative, Toast.LENGTH_SHORT).show();
         /*KonfettiView konfettiView = findViewById(R.id.viewKonfetti);
         konfettiView.build()
                         .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
@@ -250,7 +254,60 @@ public class MainActivity extends AppCompatActivity {
                         .addSizes(new Size(12, 5f))
                         .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
                         .streamFor(300, 5000L);*/
-        /*play=(Button)findViewById(R.id.button);
+        play=(Button)findViewById(R.id.button);
+        text=(TextView)findViewById(R.id.textView3);
+        play.setVisibility(View.INVISIBLE);
+        text.setVisibility(View.INVISIBLE);
+
+        GridLayout grid=(GridLayout)findViewById(R.id.gridLayout);
+        for(int i=0;i<grid.getChildCount();i++){
+            ImageView c8=(ImageView)grid.getChildAt(i);
+            c8.setImageDrawable(null);
+        }
+        for (int i=0;i<state.length;i++){
+            state[i]=2;
+        }
+        n=0;
+        w=0;
+        tagNo=0;
+        if (alternative%2==0) {
+            tagNo=0;
+            n=0;
+            Toast.makeText(getApplicationContext(), String.valueOf(alternative), Toast.LENGTH_SHORT).show();
+        }
+        else {
+            tagNo = 1;
+            n=1;
+            Toast.makeText(getApplicationContext(), String.valueOf(alternative), Toast.LENGTH_SHORT).show();
+        }
+        for (int i=0;i<tagPostion.length;i++){
+            tagPostion[i]=0;
+        }
+
+        for (Integer i=11;i<=16;i++) {
+            int resID = getResources().getIdentifier("pc"+i.toString(), "id", getPackageName());
+            ImageView p1 = findViewById(resID);
+            p1.setVisibility(View.VISIBLE);
+        }
+        for (Integer i=21;i<=26;i++) {
+            int resID = getResources().getIdentifier("pc"+i.toString(), "id", getPackageName());
+            ImageView p1 = findViewById(resID);
+            p1.setVisibility(View.VISIBLE);
+        }
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+        Intent intent=getIntent();
+        name1=intent.getStringExtra("team1");
+        name2=intent.getStringExtra("team2");
+        alternative=0;
+
+        play=(Button)findViewById(R.id.button);
         text=(TextView)findViewById(R.id.textView3);
         play.setVisibility(View.INVISIBLE);
         text.setVisibility(View.INVISIBLE);
@@ -281,27 +338,6 @@ public class MainActivity extends AppCompatActivity {
             ImageView p1 = findViewById(resID);
             p1.setVisibility(View.VISIBLE);
         }
-
-
-    }*/
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent=new Intent(this,PlayerDetails.class);
-        CountDownTimer countDownTimer=new CountDownTimer(1500,1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-
-            }
-
-            @Override
-            public void onFinish() {
-                startActivity(intent);
-                finish();
-            }
-        }.start();
-
 
 
     }
